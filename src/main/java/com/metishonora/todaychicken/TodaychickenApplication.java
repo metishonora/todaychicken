354f00a69fb685e6e90a915f8af395a62b74baf0
SG-SKKU <laino1227@gmail.com>
2023-08-15 17:43:00 +0900

    init

---
+++ src/main/java/com/metishonora/todaychicken/TodaychickenApplication.java
@@ -0,0 +1,13 @@
+package com.metishonora.todaychicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaychickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaychickenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metishonora/todaychicken/TodaychickenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metishonora.todaychicken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaychickenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
